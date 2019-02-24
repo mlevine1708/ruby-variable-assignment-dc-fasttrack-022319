1e94c4f9bb772b00c854195176c29bc570a9a89d
Micah Levine <micah.levine@outlook.com>
2019-02-24 14:58:20 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,6 +1,5 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
-def 
-  greeting_hello = "Hello World"
-puts  "#{greeting_hello}"
+def greeting_hello = "Hello World"
+puts  "greeting_hello"
  end
